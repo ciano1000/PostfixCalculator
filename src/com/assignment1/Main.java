48451c1e62817e2977da6586488ba5fd99b0d77a
CIAN MC SWEENEY <ciano1000@gmail.com>
2019-01-25 14:34:16 +0000

    Added utility method to check if users initial entry is valid

--- src/com/assignment1/Main.java
+++ src/com/assignment1/Main.java
@@ -1,8 +1,42 @@
 package com.assignment1;
-
+import javax.swing.JOptionPane;
+import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Stack stack = new ArrayStack(20);
+         String userInput = JOptionPane.showInputDialog(null,"Enter an infix notation");
+         System.out.println(isValidInput(userInput));
+    }
+
+    static boolean isValidInput(String input)
+    {
+        boolean consecDigits = false; //bool to check if consecutive digits are in the input string
+        String accepted = "-+*/()^";
+        if(input.length()>20 || input.length()<3)
+        {
+            return false;
+        }
+        for (int i=0;i<input.length();i++)
+        {
+            char c = input.charAt(i);
+            if(Character.isDigit(c))
+            {
+                if(!consecDigits)
+                {
+                    consecDigits = true;
+                }
+                else{
+                    return  false;
+                }
+                consecDigits = true;
+            }
+            else
+            {
+                consecDigits = false;
+                if (accepted.indexOf(c) < 0) return false;
+            }
+        }
+        return true;
     }
 }
